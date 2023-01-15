5a1c8a34beb8b4075fe04979161402f42eeda053
wafaselmi <wafa.selmi@insat.u-carthage.tn>
2023-01-15 18:04:54 +0100

    starting with infra

---
+++ infra/terraform/cluster/main.tf
@@ -0,0 +1,36 @@
+data "azurerm_resource_group" "dev" {
+  name = var.resource_group_name
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "pfe-se"
+  location            = data.azurerm_resource_group.dev.location
+  resource_group_name = data.azurerm_resource_group.dev.name
+  dns_prefix          = "pfe-se"
+  sku_tier            = "Free"
+
+  default_node_pool {
+    name                = "default"
+    enable_auto_scaling = true
+    min_count           = 3
+    max_count           = 5
+    vm_size             = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "kubenet"
+    network_policy = "calico"
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.kube_config_raw} >> ~/.kube/config"
+  }
+
+  tags = {
+    Environment = "Development"
+  }
+}
