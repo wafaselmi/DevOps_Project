ef4cf5b5d4529bf9f9fa8c82e85277ad91f71ba2
wafaselmi <wafa.selmi@insat.u-carthage.tn>
2023-01-16 17:11:23 +0100

    auth done

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -2,7 +2,7 @@ import os, gridfs, pika, json
 from flask import Flask, request
 from flask_pymongo import PyMongo
 from auth import validate
-from auth_sSVC import access
+from auth_SVC import access
 from storage import util
 
 server = Flask(__name__)
@@ -10,3 +10,16 @@ server.config["MONGO_URI"]= "mongodb://host.minikube.internal:27/videos"
 mongo = PyMongo(server) 
 
 fs = gridfs.GridFS(mongo.db)
+connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
+channel= connection.channel()
+@server.route("/login", methods=["POST"])
+def login():
+    token, err = access.login(request)
+    if not err:
+        return token
+    else:
+        return err
+
+@server.route("/upload", methods=["POST"])    
+def upload():
+    access, err = validate.token(request)
