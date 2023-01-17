4d4302a64c4307f6957cf15577f8f6ff207b0fc2
wafaselmi <wafa.selmi@insat.u-carthage.tn>
2023-01-17 01:16:48 +0100

    gateway service updated

--- src/gateway/server.py
+++ src/gateway/server.py
@@ -4,11 +4,18 @@ from flask_pymongo import PyMongo
 from auth import validate
 from auth_SVC import access
 from storage import util
+from bson.Objectid import ObjectId
 
 server = Flask(__name__)
-server.config["MONGO_URI"]= "mongodb://host.minikube.internal:27/videos"
-mongo = PyMongo(server) 
-fs = gridfs.GridFS(mongo.db)
+
+mongo_video = PyMongo(
+    server,
+    uri="mongodb://host.minikube.internal:27017/videos") 
+mongo_mp3 = PyMongo(
+    server,
+    uri="mongodb://host.minikube.internal:27017/mp3s") 
+fs_videos = gridfs.GridFS(mongo_video.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel= connection.channel()
 @server.route("/login", methods=["POST"])
@@ -22,13 +29,15 @@ def login():
 @server.route("/upload", methods=["POST"])    
 def upload():
     access, err = validate.token(request)
+    if err :
+        return err
     access = json.loads(access)
     if access["admin"]:
         if len(request.files) > 1 or len(request.files) < 1:
             return "exactly 1 file required", 400
         
         for _, f in request.files.items():
-            err= util.upload(f, fs, channel, access)
+            err= util.upload(f, fs_videos, channel, access)
             
             if err :
                 return err
@@ -39,6 +48,20 @@ def upload():
     
 @server.route("/download", methods=["GET"])
 def download():
-    pass
+    access, err = validate.token(request)
+    if err:
+        return err
+    access = json.loads(access)
+    if access["admin"]:
+           fid_string= request.args.get("fid")
+           if not fid_string:
+               return "fid is required", 400
+           try:
+               out=fs_mp3s.get(ObjectId(fid_string))
+               return send_file(out, download_name=f'{fid_string}.mp3')
+           except Exception as err:
+               print(err)
+               return "internal server error", 500
+    return "not authorized"  , 401  
 if __name__=="__main__":
     server.run(host="0.0.0.0", port =8000)    
